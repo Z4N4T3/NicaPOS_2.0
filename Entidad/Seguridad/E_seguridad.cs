dd20b6cc72b5a728cdfdd56521196fed036f8be6
Jurgen <jurgenq75@gmail.com>
2024-11-15 11:36:12 -0600

    Implementacion del control de acceso

---
+++ Entidad/Seguridad/E_seguridad.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidad.Seguridad
+{
+    public class E_seguridad
+    {
+    }
+
+    public class E_perfiles
+    {
+
+    }
+}
